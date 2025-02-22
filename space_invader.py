c2c0e8c6fa64dcb7c882d198374d73baa0d6b402
zephir <zephir@laptop.home>
2025-02-22 15:57:52 +0100

    change space_invader.py

--- space_invader.py
+++ space_invader.py
@@ -3,11 +3,8 @@
 import pygame
 import os
 import sys
-
-laser = pygame.image.load(os.path.join("resources/shot.png"))
-w_shot, h_shot = laser.get_size()
-
-velocity = 0
+import random
+import time
 
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, vel_x, vel_y):
@@ -29,7 +26,11 @@ class Player(Sprite):
 
 class Shot(Sprite):
     def __init__(self, player):
-        Sprite.__init__(self, pygame.image.load(os.path.join("resources/shot.png")).convert_alpha(), player.rect.midtop[0], player.rect.midtop[1], 0, -1)
+        Sprite.__init__(self, pygame.image.load(os.path.join("resources/shot.png")).convert_alpha(), player.rect.midtop[0], player.rect.midtop[1], 0, -10)
+
+class Enemy(Sprite):
+    def __init__(self):
+        Sprite.__init__(self, pygame.image.load(os.path.join("resources/alien1.png")).convert_alpha(), 550, random.choice(range(0, 850, 0)), 0, -3)
 
 print()
 print("Press any key to start")
@@ -41,6 +42,7 @@ pygame.init()
 w, h = 850, 600
 screen = pygame.display.set_mode(size=(w, h), vsync=1)
 pygame.display.set_caption('Space Invader')
+screen_rect = screen.get_rect()
 
 fps = pygame.time.Clock()
 
@@ -48,6 +50,11 @@ all_sprites = pygame.sprite.Group()
 
 ship = Player()
 all_sprites.add(ship)
+
+ship_img = pygame.image.load(os.path.join("resources/ship.png"))
+laser = pygame.image.load(os.path.join("resources/shot.png"))
+
+w_ship = ship_img.get_width()
 
 running = True
 while running:
@@ -60,16 +67,22 @@ while running:
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                ship.vel_x = -5
+                ship.vel_x = -7
             if event.key == pygame.K_RIGHT:
-                ship.vel_x = 5
+                ship.vel_x = 7
             if event.key == pygame.K_SPACE:
                 shot = Shot(ship)
+                w_shot = laser.get_width()
                 all_sprites.add(shot)
-
+                shot.rect.x, shot.rect.y = ship.rect.x - int(w_shot / 2) + int(w_ship / 2), ship.rect.y
+            if not screen_rect.contains(shot):
+                    shot.kill()
+                    print("shot deleted")
+            
     screen.fill(black)
     all_sprites.update()
     all_sprites.draw(screen)
+
     fps.tick(60)
     pygame.display.update()
 
