3718a9a5a7c59fd9cf1b073f03395aeaedb98a4e
zephir <zephir.hgfl@proton.me>
2025-03-16 14:19:18 +0100

    Update collisions + aliens spawns

--- space_invader.py
+++ space_invader.py
@@ -5,6 +5,8 @@ import os
 import sys
 import random
 
+score = 0
+
 class Sprite(pygame.sprite.Sprite):
     def __init__(self, image, x, y, vel_x, vel_y):
         pygame.sprite.Sprite.__init__(self)
@@ -23,26 +25,29 @@ class Player(Sprite):
     def __init__(self):
         Sprite.__init__(self, pygame.image.load(os.path.join("resources/ship.png")).convert_alpha(), 400, 520, 0, 0)
 
+    def update(self):
+        return super().update()
+
 class Shot(Sprite):
     def __init__(self, player):
         Sprite.__init__(self, pygame.image.load(os.path.join("resources/shot.png")).convert_alpha(), player.rect.midtop[0], player.rect.midtop[1], 0, -10)
 
+    def update(self, screen_rect):
+        if not self.rect.colliderect(screen_rect):
+            self.kill()
+
+        return super().update()
+
 class Enemy(Sprite):
     def __init__(self):
-        Sprite.__init__(self, pygame.image.load(os.path.join("resources/alien1.png")).convert_alpha(), random.choice(range(10, 850, 10)), 550, 0, 2)
+        Sprite.__init__(self, pygame.image.load(os.path.join("resources/alien1.png")).convert_alpha(), random.choice(range(10, 850, 10)), 50, 0, 1)
 
-    def laserCollision(self, alien, shot):
-        lasercollision = pygame.sprite.collide_rect(alien, shot)
-        if lasercollision == True:
-            score += 1
+    def update(self):
+        if not self.rect.colliderect(screen_rect):
+            print("\nGame over")
+            exit()
 
-    def playerCollision(self, alien, ship):
-        playercollision = pygame.sprite.collide_rect(alien, ship)
-        if playercollision == True:
-            print("Game over...")
-            print(f"Your score was : {i}")
-            pygame.quit()
-            sys.exit()
+        return super().update()
 
 print()
 print("Press any key to start")
@@ -62,16 +67,21 @@ screen_rect = screen.get_rect()
 
 fps = pygame.time.Clock()
 
-all_sprites = pygame.sprite.Group()
+player_group = pygame.sprite.Group()
+shots_group = pygame.sprite.Group()
+enemy_group = pygame.sprite.Group()
 
 ship = Player()
-all_sprites.add(ship)
+player_group.add(ship)
 
 ship_img = pygame.image.load(os.path.join("resources/ship.png"))
 laser = pygame.image.load(os.path.join("resources/shot.png"))
 
 w_ship = ship_img.get_width()
 w_shot = laser.get_width()
+
+enemy_event = pygame.event.custom_type()
+pygame.time.set_timer(enemy_event, 500)
 
 score = 0
 
@@ -91,27 +101,34 @@ while running:
                 ship.vel_x = 7
             if event.key == pygame.K_SPACE:
                 shot = Shot(ship)
-                all_sprites.add(shot)
                 shot.rect.x, shot.rect.y = ship.rect.x - int(w_shot / 2) + int(w_ship / 2), ship.rect.y
+                shots_group.add(shot)
 
-                if shot.rect.y < h:
-                    shot.kill()
-                    print("shot deleted")
+        if event.type == enemy_event:
+            alien = Enemy()
+            enemy_group.add(alien)
 
     screen.fill(black)
-    all_sprites.update()
-    all_sprites.draw(screen)
 
-    alien = Enemy()
-    all_sprites.add(alien)
-    alien.laserCollision(alien, shot)
-    alien.playerCollision(alien, ship)
+    player_group.update()
+    shots_group.update(screen_rect)
+
+    if pygame.sprite.groupcollide(shots_group, enemy_group, True, True, collided=None):
+        score += 10
+
+    if pygame.sprite.groupcollide(player_group, enemy_group, True, True, collided=None):
+        print("\nGame over")
+        break
+
+    player_group.draw(screen)
+    shots_group.draw(screen)
+
+    enemy_group.update()
+    enemy_group.draw(screen)
 
     fps.tick(60)
     pygame.display.update()
-    
-    for i in score:
-        print(f"Your score : {i}")
 
+print(f"Your score was : {score}")
 pygame.quit()
 sys.exit()
